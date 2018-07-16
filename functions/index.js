d9db3553b7dd915e084f73f26eb4ad839df9793a
3sho7mi8 <hiro.pinot@gmail.com>
2018-07-16 19:00:54 +0900

    Add POST /channels

--- functions/index.js
+++ functions/index.js
@@ -18,3 +18,46 @@ const app = express();
 const cors = require('cors')({origin: true});
 
 app.use(cors);
+
+// 引数cnameでRealtimeDatabaseのパスchannels/:cnameにデータを挿入
+// Admin SDkによるRealtimeDatabaseの操作 => admin.database()
+//   特定のノードを参照する場合は.refをつける
+//   子ノードの参照は.child()
+//   データの挿入は.set()
+function createChannel(cname){
+  let channelsRef = admin.database().ref('channels');
+  let date1 = new Date();
+  let date2 = new Date();
+  date2.setSeconds(date2.getSeconds() + 1);
+
+  const defaultData = `{
+    "messages" : {
+      "1" : {
+        "body" : "Welcome to #${cname} channel!",
+        "data" : "${date1.toJSON()}",
+        "user" : {
+          "avatar" : "",
+          "id" : "robot",
+          "name" : "Robot"
+        }
+      },
+      "2" : {
+        "body" : "はじめてのメッセージを投稿してみましょう。",
+        "date" : "${date2.toJSON()}",
+        "user" : {
+          "avatar" : "".
+          "id" : "robot",
+          "name" : "Robot"
+        }
+      }
+    }
+  }`;
+  channelsRef.child(cname).set(JSON.parse(defaultData));
+}
+
+app.post('/channels', (req, res) => {
+  let cname = req.body.cname;
+  createChannel(cname);
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.status(201).json({result: 'ok'});
+});
