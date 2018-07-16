5c8c142ff1d2731f51a1f07d730aa70c55446225
3sho7mi8 <hiro.pinot@gmail.com>
2018-07-16 18:38:01 +0900

    Add Admin SDK / Import Express module and CORS module

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
+// Firebase Functions SDK
+// HTTPリクエストをトリガするために使用
 const functions = require('firebase-functions');
+
+// Firebase Admin SDK
+// Realtime Databaseの処理および認証をするために使用
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +12,9 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const express = require('express');
+const app = express();
+const cors = require('cors')({origin: true});
+
+app.use(cors);
